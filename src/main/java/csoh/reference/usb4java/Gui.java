8fcfc72125d7fefd30f71c26af7f0b18deca17eb
Calvin <soh_calvin@yahoo.com>
2015-11-29 22:41:55 +0800

    Added GUI interface

---
+++ src/main/java/csoh/reference/usb4java/Gui.java
@@ -0,0 +1,9 @@
+package csoh.reference.usb4java;
+
+public interface Gui {
+    
+    void setProgressLevel(int level);
+
+    void setMessageFromClient(String mess);
+    
+}
---
+++ src/main/java/csoh/reference/usb4java/GuiImpl.java
@@ -0,0 +1,40 @@
+package csoh.reference.usb4java;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class GuiImpl implements Gui {
+    private WebsocketClientEndpoint clientEndPoint = null; 
+    public GuiImpl() {
+	try {
+	    // open websocket
+	    clientEndPoint = new WebsocketClientEndpoint(new URI("ws://localhost:9000"));
+
+	    // add listener
+	    clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
+		public void handleMessage(String message) {
+		    System.out.println("recv:" + message);
+		}
+	    });
+	   
+
+	} catch (URISyntaxException ex) {
+	    System.err.println("URISyntaxException exception: " + ex.getMessage());
+	}
+    }
+
+    @Override
+    public void setProgressLevel(int level) {
+	 // send message to websocket
+	    clientEndPoint.sendMessage("INC");
+
+    }
+
+    @Override
+    public void setMessageFromClient(String mess) {
+	// TODO Auto-generated method stub
+
+    }
+
+   
+}
