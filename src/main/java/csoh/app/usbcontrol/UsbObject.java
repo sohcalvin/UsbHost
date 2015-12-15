12e28ddc5cc93b30e9e27e40aae6da2f2469599f
Calvin <soh_calvin@yahoo.com>
2015-12-15 23:29:51 +0800

    Free device list after device open

--- src/main/java/csoh/app/usbcontrol/UsbObject.java
+++ src/main/java/csoh/app/usbcontrol/UsbObject.java
@@ -21,6 +21,7 @@ public class UsbObject {
 
     private int interfaceNum = 1;
     private Device device;
+    private DeviceList deviceList;
     private DeviceHandle handle;
     public DeviceHandle getHandle() {
         return handle;
@@ -37,6 +38,7 @@ public class UsbObject {
 	handle = new DeviceHandle();
 
 	int result = LibUsb.open(device, handle);
+	LibUsb.freeDeviceList(deviceList, true);
 	if (result != LibUsb.SUCCESS)  throw new LibUsbException("Unable to open USB device", result);
 
 	detachKernelDriverIfKernelDriverActive();
@@ -76,18 +78,19 @@ public class UsbObject {
 	// LibUsbException("Unable to setAutoDetachedKernelDriver because it is not supported",supported);
 	// }
     }
-    private static Device findDevice(short vendorId) {
+    private  Device findDevice(short vendorId) {
 	return findDevice(vendorId, PRODUCT_ID_DONT_CARE);
     }
-    private static Device findDevice(short vendorId, short productId) {
+    private  Device findDevice(short vendorId, short productId) {
 	// Read the USB device list
-	DeviceList list = new DeviceList();
-	int result = LibUsb.getDeviceList(null, list);
+	//DeviceList list = new DeviceList();
+	deviceList = new DeviceList();
+	int result = LibUsb.getDeviceList(null, deviceList);
 	if (result < 0) throw new LibUsbException("Unable to get device list", result);
 
 	try {
 	    // Iterate over all devices and scan for the right one
-	    for (Device device : list) {
+	    for (Device device : deviceList) {
 		DeviceDescriptor descriptor = new DeviceDescriptor();
 		result = LibUsb.getDeviceDescriptor(device, descriptor);
 		if (result != LibUsb.SUCCESS)    throw new LibUsbException("Unable to read device descriptor", result);
@@ -104,7 +107,7 @@ public class UsbObject {
 	} finally {
 	    // Ensure the allocated device list is freed
 	   	    
-	    LibUsb.freeDeviceList(list, true);
+	    //LibUsb.freeDeviceList(list, true);
 	}
 	// Device not found
 	return null;
